f5dbddccb36fd018a5a407a1f776487ec374418d
slifer <dara51php@gmail.com>
2017-09-20 23:14:05 +0430

    asd

--- cmd/cloud/main.go
+++ cmd/cloud/main.go
@@ -1,19 +1,31 @@
 package main
 
 import (
-	"github.com/sirupsen/logrus"
-	"k8s.io/client-go/rest"
+	"fmt"
+
 	"github.com/clickyab/services/assert"
+	"github.com/sirupsen/logrus"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
 
 func main() {
 	// get cluster config
 	config, err := rest.InClusterConfig()
 	assert.Nil(err)
-	logrus.Warn(config)
-
-	clientSet,err:=kubernetes.NewForConfig(config)
+	clientSet, err := kubernetes.NewForConfig(config)
 	assert.Nil(err)
-	logrus.Warn(clientSet)
+	nodes, err := clientSet.CoreV1().Nodes().List(metav1.ListOptions{})
+	assert.Nil(err)
+	for _, n := range nodes.Items {
+		for _, s := range n.Status.Addresses {
+			logrus.Warn(fmt.Sprintf("Address: %s, Type: %s", s.Address, s.Type))
+		}
+		for _, s := range n.Status.Conditions {
+			logrus.Warn(fmt.Sprintf("Type: %s, status: %s, Message: %s", s.Type, s.Message, s.Status))
+		}
+		logrus.Info("//////////////////////////")
+
+	}
 }
